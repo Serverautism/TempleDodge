4d642db8f3606de29e424a65b78833930b7c9b59
serverautism <adamsagthi@gmail.com>
2022-01-22 12:31:53 +0100

    Commit Message

--- Data/Scripts/item.py
+++ Data/Scripts/item.py
@@ -7,30 +7,45 @@ from . import particle
 from . import funcs
 
 
+# this object represents a gold coin or a mana orb
 class Item:
+    # set the attributes
     def __init__(self, name, center, velocity, landed_rocks, falling_rocks):
+        # the name it the type of item
         self.name = name
+
         self.center = center
         self.velocity = velocity
         self.gravity = 0.2
+
+        # lists with all the rocks for collision detection
         self.landed_rocks = landed_rocks
         self.falling_rocks = falling_rocks
+
+        # variables to change the frames of the animation
         self.frames_count = 0
         self.frames_change = 5
         self.frame = randint(0, 5)
+
+        # state of the item
         self.collected = False
         self.collectable = False
+
+        # count for the collectable delay after being spawned
         self.collectable_delay = 20
         self.collectable_delay_count = 0
 
         self.particles = []
 
+        # variables for spawning particles
         self.move_particle_time = 10
         self.move_particle_count = 0
 
+        # these two rects represent the left and right map border
         self.left_border_rect = pygame.rect.Rect(0, 0, 16, 288)
         self.right_border_rect = pygame.rect.Rect(512 - 16, 0, 16, 288)
 
+        # set parameters for the animation and particle color based on the type of the item
         if self.name == 'mana':
             self.frames_len = 15
             path = 'Data/Assets/Sprites/Items/Mana/mana_'
@@ -42,58 +57,69 @@ class Item:
             self.move_particle_color = (224, 192, 95)
             self.move_particle_glow_color = (14, 10, 0)
 
+        # load the animation frames into a list
         self.frames = []
         for i in range(self.frames_len):
             image = pygame.image.load(path + f'{i + 1}.png').convert_alpha()
             self.frames.append(image)
 
+        # set the image to the first frame and set the hit box and the mask for pixel perfect collision
         self.image = self.frames[self.frame]
         self.rect = self.image.get_rect()
         self.rect.center = self.center
         self.mask = pygame.mask.from_surface(self.image)
 
+        # track time between frames
         self.dt = 0
         self.last_time = time.time()
 
+        # load the crush sound and set its volume
         self.crush_sound = pygame.mixer.Sound('Data/Assets/Sound/Sfx/item_crush_1.wav')
         self.crush_sound.set_volume(.1)
 
+    # main update function
     def update(self, surface, paused):
-        # calculate delta time
+        # calculate delta time since the last frame
         self.dt = time.time() - self.last_time
         self.dt *= 60
         self.last_time = time.time()
 
-        # get image
+        # get the current image
         self.get_image()
 
         if not paused:
-            # check if collectable
+            # check if the item is collectable
             if not self.collectable:
                 self.check_collectable()
 
-            # move left right, check collision
+            # move left right, set the hit box and check collision
             self.center[0] += self.velocity[0] * self.dt
             self.rect.centerx = self.center[0]
             self.check_collision_x()
 
-            # move up down, check collision
+            # move up down, set the hit box and check collision
             self.center[1] += self.velocity[1] * self.dt
             self.rect.centery = self.center[1]
             self.check_collision_y()
+
+            # apply gravity to the vertical movement
             self.apply_gravity()
 
-        # blit
+        # draw the image to the surface
         surface.blit(self.image, self.rect)
 
+    # checks the collision on the x-axis
     def check_collision_x(self):
+        # get all the landed rocks the item collides with
         landed_rock_hit_list = pygame.sprite.spritecollide(self, self.landed_rocks, False)
+
+        # if there is at least one we undo the movement from the update function, set the hit box and change the movement direction + slow the movement down
         if len(landed_rock_hit_list) > 0:
             self.center[0] -= self.velocity[0] * self.dt
             self.rect.centerx = self.center[0]
-
             self.velocity[0] *= -.75
 
+        # do the same thing for the falling rocks...
         falling_rock_hit_list = pygame.sprite.spritecollide(self, self.falling_rocks, False)
         if len(falling_rock_hit_list) > 0:
             if self.velocity[0] != 0:
@@ -101,72 +127,93 @@ class Item:
                 self.rect.centerx = self.center[0]
             self.velocity[0] *= -.75
 
+        # ...and the same thing for the left map border...
         if self.rect.colliderect(self.left_border_rect):
             self.rect.left = self.left_border_rect.right
             self.center = list(self.rect.center)
             self.velocity[0] *= -.75
 
+        # ...and the right map border
         if self.rect.colliderect(self.right_border_rect):
             self.rect.right = self.right_border_rect.left
             self.center = list(self.rect.center)
             self.velocity[0] *= -.75
 
+    # checks for collision on the y-axis
     def check_collision_y(self):
+        # check for collision with landed rocks
         landed_rock_hit_list = pygame.sprite.spritecollide(self, self.landed_rocks, False)
+
+        # if there is at least one we undo the movement from the update function, set the hit box and change the movement direction + slow the movement down
         if len(landed_rock_hit_list) > 0:
             if self.velocity[1] != 0:
                 self.center[1] -= self.velocity[1] * self.dt
                 self.rect.centery = self.center[1]
             self.velocity[1] *= -.25
 
+        # for the falling rocks, do basically the same thing but...
         falling_rock_hit_list = pygame.sprite.spritecollide(self, self.falling_rocks, False)
         for entity in falling_rock_hit_list:
+            # ...if the item is under the rock it gets destroyed
             if self.rect.y >= entity.rect.y:
                 self.collected = True
                 self.crush_sound.play()
+            # otherwise it bounces away
             else:
                 self.center[1] -= self.velocity[1] * self.dt
                 self.rect.centery = self.center[1]
                 self.velocity[1] *= -.25
 
+    # this function adds the gravity to the movement speed on the y-axis
     def apply_gravity(self):
-        if self.velocity[1] != 0:
-            self.velocity[1] += self.gravity * self.dt
+        self.velocity[1] += self.gravity * self.dt
 
+    # this function counts the time since the item is being spawned
     def check_collectable(self):
         self.collectable_delay_count += 1 * self.dt
+
+        # and makes it collectable to the player
         if self.collectable_delay_count >= self.collectable_delay:
             self.collectable = True
 
+    # this function is being called by the player on collision
     def collect(self):
         self.collected = True
 
+    # get the current image from the animation
     def get_image(self):
+        # count the time since the last frame and update the frame number
         self.frames_count += 1 * self.dt
         if self.frames_count >= self.frames_change:
             self.frames_count = 0
             self.frame += 1
+
+            # and reset the frame number at the and of the animation
             if self.frame > self.frames_len - 1:
                 self.frame = 0
 
-        self.image = self.frames[self.frame]
-        self.mask = pygame.mask.from_surface(self.image)
+            # get the new image from the animation list and update the mask for the pixel perfect animation
+            self.image = self.frames[self.frame]
+            self.mask = pygame.mask.from_surface(self.image)
 
+    # this function updates and spawns the particles
     def update_particles(self, surface):
-        # spawn particle
+        # spawn particle if its the time to do so
         self.move_particle_count += 1 * self.dt
         if self.move_particle_count >= self.move_particle_time:
             self.move_particle_count = 0
 
+            # set the particles attributes
             center = list(funcs.render_pos_to_screen_pos(self.rect.center, (1920, 1080)))
             velocity = [randint(1, 10) / 10 - .5, randint(1, 10) / 10 - .5]
             radius = randint(1, 3)
             lifetime = 1
 
+            # create a new particle object and add it to the list
             p = particle.Particle(center, velocity, radius, lifetime, self.move_particle_color, self.move_particle_glow_color, has_glow=True)
             self.particles.append(p)
 
-        # update_particles
+        # update the particles and remove them if they are dead
         to_remove = []
         for entity in self.particles:
             if entity.dead:
--- Data/Scripts/particle.py
+++ Data/Scripts/particle.py
@@ -2,79 +2,99 @@ import pygame
 import time
 
 
+# this object represents one of the particles used by many objects in the game
 class Particle:
-    def __init__(self, center, velocity, radius, lifetime, color, glow_color, static_size=False, has_glow=False, rocks=None,
-                 gravity=0, glow_scale=5):
+    def __init__(self, center, velocity, radius, lifetime, color, glow_color, static_size=False, has_glow=False, rocks=None, gravity=0, glow_scale=5):
         self.center = center
         self.velocity = velocity
         self.radius = radius
+
+        # lifetime is the duration before the particle dies
         self.lifetime = lifetime * 60
+
         self.color = color
         self.glow_color = glow_color
+
+        # does the particle keeps its size over its lifetime?
         self.static_size = static_size
+
         self.has_glow = has_glow
+
+        # used for physics if the particle has them enabled
         self.rock = rocks
+
         self.gravity = gravity
+
+        # size of the glow compared to the radius of the particle
         self.glow_scale = glow_scale
 
         self.dead = False
 
+        # track them time since the last frame
         self.last_time = time.time()
         self.dt = 0
 
+        # if the particle does not change its size we pre render the glow image
         if self.static_size and self.has_glow:
             self.glow_image = self.get_glow()
+
+        # else if the particle changes size over time we calculate the change per frame
+        # so that its size is 0 when it dies
         elif not self.static_size:
             self.radius_change = self.radius / self.lifetime
 
+    # main update function
     def update(self, surface):
-        # calc delta time
+        # calculate the delta time since the last frame
         self.dt = time.time() - self.last_time
         self.dt *= 60
         self.last_time = time.time()
 
-        # change position
+        # change position based on velocity and the time passed
         self.center[0] += self.velocity[0] * self.dt
         self.center[1] += self.velocity[1] * self.dt
 
         # interact with rocks
 
-        # apply gravity
+        # apply gravity to the movement on the y-axis
         self.velocity[1] += self.gravity * self.dt
 
-        # change radius
+        # change radius if the particle is not static in its size
         if not self.static_size:
             self.radius -= self.radius_change * self.dt
             if self.radius <= 0:
                 self.dead = True
                 return
+
+        # otherwise we just count down the lifetime
         else:
             self.lifetime -= 1 * self.dt
             if self.lifetime <= 0:
                 self.dead = True
 
-        # draw glow
+        # if the particle has glow enabled
         if self.has_glow:
+            # we draw the pre rendered image for static sizes
             if self.static_size:
                 surface.blit(self.glow_image, (int(self.center[0]) - int(self.radius * self.glow_scale), int(self.center[1]) - int(self.radius * self.glow_scale)), special_flags=pygame.BLEND_RGB_ADD)
+
+            # or generate a new glow image with the changed radius
             else:
                 surface.blit(self.get_glow(), (int(self.center[0]) - int(self.radius * self.glow_scale), int(self.center[1]) - int(self.radius * self.glow_scale)), special_flags=pygame.BLEND_RGB_ADD)
 
-        # draw particle
+        # draw the particle
         pygame.draw.circle(surface, self.color, [int(self.center[0]), int(self.center[1])], int(self.radius))
 
+    # renders the glow
     def get_glow(self):
+        # calculate the radius of the glow
         glow_radius = int(self.radius * self.glow_scale)
+
+        # create a new surface to draw the radius on
         surface = pygame.Surface((glow_radius * 2, glow_radius * 2)).convert_alpha()
         surface.set_colorkey((0, 0, 0))
-        pygame.draw.circle(surface, self.glow_color, (glow_radius, glow_radius), glow_radius)
-        return surface
 
-    def get_glow_color(self):
-        r, g, b = self.color
-        min_val = min([i for i in self.color if i > 0])
-        diff = 20 - min_val
-        if diff < 0:
-            return r - abs(diff), g - abs(diff), b - abs(diff)
-        else:
-            return self.color
+        # then draw the glow as a circle onto the new surface
+        pygame.draw.circle(surface, self.glow_color, (glow_radius, glow_radius), glow_radius)
+
+        return surface
