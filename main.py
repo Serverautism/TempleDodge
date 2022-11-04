6304944bf0b3a890e5dc52570c6a8e5c6256b60f
serverautism <adamsagthi@gmail.com>
2022-11-04 09:14:36 +0100

    Commit Message

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ class Game:
         while self.running:
             # clock keeps the game from overshoot the fps
             self.clock.tick(self.fps)
-            print(self.clock.get_fps())
+            # print(self.clock.get_fps())
 
             # determine delta time which is mostly used with movement to compensate slower fps
             self.dt = time.time() - self.last_time
